1371b069008066ed02a6901e0305264ff0a133c1
Johnny5191 <80360008+Johnny5191@users.noreply.github.com>
2022-09-28 18:32:37 -0300

    POST, PUT e DELETE

--- app/Controllers/ProdutoController.php
+++ app/Controllers/ProdutoController.php
@@ -20,3 +20,23 @@ class ProdutoController extends ResourceController
 
     }
 }
+ public function gravarProduto(){
+        $this->produtosModel->save([
+            'nome' => $this->request->getPost('nome'),
+            'descricao' => $this->request->getPost('descricao'),
+            'cor' => $this->request->getPost('cor'),
+            'cobertura' => $this->request->getPost('cobertura'),
+            'tamanho' => $this->request->getPost('tamanho'),
+            'valor' => $this->request->getPost('valor')
+        ]);
+    }
+
+    public function deletarProduto($id){
+        $this->produtosModel->delete($id);
+    }
+
+    public function atualizarProduto($id){
+        $data = $this->request->getJSON();
+        $this->produtosModel->update($id, $data);
+    }
+}
